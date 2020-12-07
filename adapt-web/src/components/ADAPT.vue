84726c0d63bf3e9d4ae08e976bc266c22da67062
Priya P. Pillai <34429433+priyappillai@users.noreply.github.com>
2020-12-07 12:52:50 -0600

    Adding Django
    Integrated Django and Vue (using this tutorial: https://medium.com/js-dojo/vue-django-best-of-both-frontends-701307871478)

--- adapt-web/src/components/ADAPT.vue
+++ adapt-web/src/components/ADAPT.vue
@@ -4,12 +4,24 @@
     <button @click="count++">
       You clicked me {{ count }} times.
     </button>`
-    <button @click="greet">Greet</button>
+
+    <button @click="adapt_run">Run ADAPT on test example</button>
+    <h3>Objectives</h3>
+    <input type="checkbox" id="max_act" value="maximum_activity" v-model="objs">
+    <label for="max_act">Maximum Activity</label>
+    <br>
+    <input type="checkbox" id="min_guide" value="minimum_guides" v-model="objs">
+    <label for="min_guide">Minimum Guides</label>
+    <br>
+    <span>Picked: {{ objs }}</span>
+
   </div>
 </template>
 
 <script>
-const axios = require("axios");
+// const axios = require("axios");
+const FormData = require('form-data');
+// const https = require('https');
 
 export default {
   name: 'ADAPT',
@@ -19,14 +31,56 @@ export default {
   data () {
     return {
       count: 0,
-      name: 'ADAPT'
+      name: 'ADAPT',
+      objs : []
     }
   },
   methods: {
-    async greet(event) {
-      const { data } = await axios.get("https://api.agify.io/?name=Priya")
+    async adapt_run(event) {
+      const form = new FormData();
+      const json = JSON.stringify({
+        "single_adapt.adapt.queueArn": "None",
+        "single_adapt.adapt.taxid": 64320,
+        "single_adapt.adapt.ref_accs": "NC_035889",
+        "single_adapt.adapt.segment": "None",
+        "single_adapt.adapt.obj": "minimize-guides",
+        "single_adapt.adapt.specific": false,
+        "single_adapt.adapt.image": "quay.io/broadinstitute/adaptcloud",
+        "single_adapt.adapt.rand_sample": 5,
+        "single_adapt.adapt.rand_seed": 294
+      });
+      form.append('workflowUrl', 'https://github.com/broadinstitute/adapt-pipes/blob/master/single_adapt.wdl');
+      form.append('workflowInputs', json);
+      // const agent = new https.Agent({  
+      //   rejectUnauthorized: false
+      // });
+
+      // var xhr = new XMLHttpRequest();
+      // xhr.withCredentials = true;
+
+      // xhr.addEventListener("readystatechange", function () {
+      //   if (this.readyState === 4) {
+      //     console.log(this.responseText);
+      //   }
+      // });
+
+      // xhr.open("POST", "https://connect.stripe.com/oauth/token");
+      // xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+      // xhr.setRequestHeader("cache-control", "no-cache");
+
+      // xhr.send(form);
+
+      const response = await fetch('http://ec2-54-91-18-102.compute-1.amazonaws.com/api/workflows/v1', {
+        method: 'post',
+        body: form,
+      })
+      const data = response.json()
+      // const { data } = await axios.post("https://ec2-54-91-18-102.compute-1.amazonaws.com/api/workflows/v1", form, {
+      //   headers: {'Content-Type': 'multipart/form-data'},
+      //   httpsAgent: agent
+      // })
       // `this` inside methods points to the current active instance
-      alert('Hello ' + data.name + '!')
+      alert('Job submitted ' + data.name + '!')
       // `event` is the native DOM event
       if (event) {
         alert(event.target.tagName)
--- adapt-web/vue.config.js
+++ adapt-web/vue.config.js
@@ -1,36 +1,84 @@
+const BundleTracker = require("webpack-bundle-tracker")
+
+const pages =  {
+  index: {
+    // entry for the page
+    entry: './src/pages/Home/main.js',
+    // the source template
+    template: 'public/index.html',
+    // output as dist/index.html
+    filename: 'index.html',
+    // when using title option,
+    // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+    title: 'Home Page',
+    // chunks to include on this page, by default includes
+    // extracted common chunks and vendor chunks.
+    chunks: ['chunk-vendors', 'chunk-common', 'index']
+  },
+  // when using the entry-only string format,
+  // template is inferred to be `public/subpage.html`
+  // and falls back to `public/index.html` if not found.
+  // Output filename is inferred to be `subpage.html`.
+  designs: {
+    entry: './src/pages/Designs/main.js',
+    template: 'public/index.html',
+    filename: 'designs.html',
+    title: 'Designs',
+    chunks: ['chunk-vendors', 'chunk-common', 'designs']
+  },
+  adapt: {
+    entry: './src/pages/ADAPT/main.js',
+    template: 'public/index.html',
+    filename: 'adapt.html',
+    title: 'ADAPT',
+    chunks: ['chunk-vendors', 'chunk-common', 'adapt']
+  }
+}
+
 module.exports = {
-  pages: {
-    index: {
-      // entry for the page
-      entry: './src/pages/Home/main.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
-      filename: 'index.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Home Page',
-      // chunks to include on this page, by default includes
-      // extracted common chunks and vendor chunks.
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
-    // when using the entry-only string format,
-    // template is inferred to be `public/subpage.html`
-    // and falls back to `public/index.html` if not found.
-    // Output filename is inferred to be `subpage.html`.
-    designs: {
-      entry: './src/pages/Designs/main.js',
-      template: 'public/index.html',
-      filename: 'designs.html',
-      title: 'Designs',
-      chunks: ['chunk-vendors', 'chunk-common', 'designs']
-    },
-    adapt: {
-      entry: './src/pages/ADAPT/main.js',
-      template: 'public/index.html',
-      filename: 'adapt.html',
-      title: 'ADAPT',
-      chunks: ['chunk-vendors', 'chunk-common', 'adapt']
-    }
+  pages: pages,
+  filenameHashing: false,
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? ''
+    : 'http://localhost:8080/',
+  outputDir: '../django_adapt_web/static/vue/',
+
+  chainWebpack: config => {
+
+    config.optimization
+      .splitChunks({
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "chunk-vendors",
+            chunks: "all",
+            priority: 1
+          },
+        },
+      });
+
+    Object.keys(pages).forEach(page => {
+      config.plugins.delete(`html-${page}`);
+      config.plugins.delete(`preload-${page}`);
+      config.plugins.delete(`prefetch-${page}`);
+    })
+
+    config
+      .plugin('BundleTracker')
+      .use(BundleTracker, [{filename: '../adapt-web/webpack-stats.json'}]);
+
+    config.resolve.alias
+        .set('__STATIC__', 'static')
+
+    config.devServer
+        .public('http://localhost:8080')
+        .host('localhost')
+        .port(8080)
+        .hotOnly(true)
+        .watchOptions({poll: 1000})
+        .https(false)
+        .headers({"Access-Control-Allow-Origin": ["*"]})
+
   }
 }
