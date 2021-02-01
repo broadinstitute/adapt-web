d667c95c9b50045d8b764c2fb3614cf9be44a34c
Priya P. Pillai <34429433+priyappillai@users.noreply.github.com>
2021-02-01 12:44:15 -0500

    Make script to start server, small css modifications

---
+++ start.sh
@@ -0,0 +1,16 @@
+yarn --cwd ./adapt-web build
+
+for cmd in "python manage.py runserver" "yarn --cwd ./adapt-web serve"; do {
+  echo "Process \"$cmd\" started";
+  $cmd & pid=$!
+  PID_LIST+=" $pid";
+} done
+
+trap "kill $PID_LIST" SIGINT
+
+echo "Parallel processes have started";
+
+wait $PID_LIST
+
+echo
+echo "All processes have completed";
