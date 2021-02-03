dc18d48fb30a8041be45e36655ba90171c09e512
Priya P. Pillai <34429433+priyappillai@users.noreply.github.com>
2021-02-03 16:11:47 -0500

    Add Lightsail IP
    Add Lightsail Static IP as a host

--- django_adapt_web/settings.py
+++ django_adapt_web/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'w3-k9sdlk51y7sf08%cq!hyu4_zb&!ruk9di1dzgf$+lqhgte*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["52.22.166.32"]
+ALLOWED_HOSTS = [
+    "52.22.166.32",
+    "34.231.167.163"
+]
 
 
 # Application definition
