b64389c7b493350fe7e07d088b00c5070a038bbe
Priya P. Pillai <34429433+priyappillai@users.noreply.github.com>
2021-02-08 12:19:25 -0500

    Update setup_linux.sh
    Check if AWS config file exists before creating it

--- setup_linux.sh
+++ setup_linux.sh
@@ -8,6 +8,12 @@ sudo apt install python3-pip
 sudo npm install -g vue@next
 sudo yarn --cwd vue_frontend/ install
 sudo pip install -r requirements.txt || sudo pip3 install -r requirements.txt
-touch api/aws_config.txt
-echo 'INSERTACCESSKEY' >> api/aws_config.txt
-echo 'INSERTSECRETACCESSKEY' >> api/aws_config.txt
+FILE=api/aws_config.txt
+if [[ -f "$FILE" ]]; then
+    echo "$FILE exists."
+else
+    echo "$FILE does not exist; making a template. Fill in with AWS Access Keys."
+    touch $FILE
+    echo 'INSERTACCESSKEY' >> api/aws_config.txt
+    echo 'INSERTSECRETACCESSKEY' >> api/aws_config.txt
+fi
