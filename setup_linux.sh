d9e1f25390708fada51a5828bf719002ff823398
Priya P. Pillai <34429433+priyappillai@users.noreply.github.com>
2021-02-11 05:06:01 -0500

    Fix vue install
    Remove vue install from setup_linux.sh since it's in the yarn install anyway

--- setup_linux.sh
+++ setup_linux.sh
@@ -3,7 +3,6 @@ sudo apt install npm
 sudo apt install python3.8
 sudo apt install python3-pip
 sudo npm install --g yarn
-sudo npm install -g vue@next
 sudo yarn --cwd vue_frontend/ install
 sudo pip install -r requirements.txt || sudo pip3 install -r requirements.txt
 FILE=api/aws_config.txt
