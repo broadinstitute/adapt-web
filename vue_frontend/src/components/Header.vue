5abd8ee7326fbc2d423a1b5f85b4bb5f05b3bf47
Priya P. Pillai <34429433+priyappillai@users.noreply.github.com>
2021-09-27 16:10:15 -0400

    Bigger header font for navbar items

--- vue_frontend/src/components/Header.vue
+++ vue_frontend/src/components/Header.vue
@@ -8,13 +8,13 @@
       </svg>
     </a>
     <b-row>
-      <b-col cols=0 md=2></b-col>
-      <b-col cols=12 md=8>
-      <b-navbar toggleable="lg" class="px-4 py-2">
+      <b-col cols=0 md=1></b-col>
+      <b-col cols=12 md=10>
+      <b-navbar toggleable="lg">
         <b-navbar-brand href="/">AD<img v-if="prod" class="logo" @load="onLoad" :src="`${imgsrc}/static/vue/img/logo.png`" alt="A"><img v-else class="logo" @load="onLoad" src="@/assets/img/logo.png" alt="A">PT</b-navbar-brand>
         <b-navbar-toggle target="nav-collapse">
         </b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav><b-navbar-nav :justified=true class="w-100 ml-auto pr-2">
+        <b-collapse id="nav-collapse" is-nav><b-navbar-nav :justified=true class="w-100 mx-auto">
         <b-nav-item disabled href=""></b-nav-item>
         <b-nav-item href="/designs" :class="{ 'current-page': page=='Assays'}">PRE-DESIGNED ASSAYS</b-nav-item>
         <b-nav-item href="/run" :class="{ 'current-page': page=='Run'}">RUN ADAPT</b-nav-item>
@@ -22,7 +22,7 @@
         </b-navbar-nav></b-collapse>
       </b-navbar>
       </b-col>
-      <b-col cols=0 md=2></b-col>
+      <b-col cols=0 md=1></b-col>
     </b-row>
   </b-container>
 </template>
