eaef2bc671df7a34517f5884f886f750bdda9c45
Priya P. Pillai <34429433+priyappillai@users.noreply.github.com>
2022-03-02 21:52:39 -0500

    Add breaks to cases
    Forgot to save...oops

--- vue_frontend/src/components/Results.vue
+++ vue_frontend/src/components/Results.vue
@@ -211,12 +211,16 @@ export default {
             switch(detail_response_json) {
               case 'Memory':
                 this.$root.$data.modalmsg = this.$root.$data.modalmsg.concat('running out of memory. Try increasing the memory setting in the "Advanced" options on the Run page and then submitting again.')
+              break;
               case 'No sequences':
                 this.$root.$data.modalmsg = this.$root.$data.modalmsg.concat('our database of viral sequences (<a href="https://www.ncbi.nlm.nih.gov/genomes/GenomesGroup.cgi?taxid=10239">NCBI\'s Viral Genome Database</a>) not containing any complete genomes for this virus. Try uploading a FASTA of the genomes you would like to detect.')
+              break;
               case 'Busy':
                 this.$root.$data.modalmsg = this.$root.$data.modalmsg.concat('our servers being busy. Try waiting for a few hours and then submitting again.')
+              break;
               case 'Unknown':
                 this.$root.$data.modalmsg = this.$root.$data.modalmsg.concat('an unknown reason. Please double check your input parameters; if you uploaded a file, this could be due to incorrect formatting.')
+              break;
             }
             this.$root.$data.modalmsg = this.$root.$data.modalmsg.concat(' If you continue to have issues, contact adapt@broadinstitute.org with your run ID.')
             this.$root.$data.modalvariant = 'danger';
