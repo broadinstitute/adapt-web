702a941e0e5a1b1607ba8e51abd113437d507efc
Priya P. Pillai <34429433+priyappillai@users.noreply.github.com>
2022-03-22 07:46:21 -0400

    Change functions to properties in taxonremove.py

--- api/management/commands/taxonremove.py
+++ api/management/commands/taxonremove.py
@@ -11,10 +11,10 @@ class Command(BaseCommand):
         self.stdout.write('Running...')
 
         for tax in Taxon.objects.all():
-            if not (tax.taxonrank.any_assays() or tax.taxonrank.any_child_assays()):
+            if not (tax.taxonrank.any_assays or tax.taxonrank.any_child_assays):
                 tax.delete()
         for taxrank in TaxonRank.objects.all():
-            if not (taxrank.any_assays() or taxrank.any_child_assays()):
+            if not (taxrank.any_assays or taxrank.any_child_assays):
                 taxrank.delete()
 
         self.stdout.write(self.style.SUCCESS('Successfully removed unused taxons'))
