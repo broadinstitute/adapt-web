bb6ed0a56de5c27a648589e513985f9f492da2a6
Priya P. Pillai <34429433+priyappillai@users.noreply.github.com>
2022-03-23 17:16:37 -0400

    Have Run ADAPT page use pagination to load taxons

--- vue_frontend/src/components/RunADAPT.vue
+++ vue_frontend/src/components/RunADAPT.vue
@@ -341,32 +341,36 @@ export default {
   async created() {
     let vm = this
     for (let filterrank of ['species', 'genus', 'subspecies', 'family']) {
-      let response = await fetch('/api/taxon/?rank='+filterrank, {
-        headers: {
-          "X-CSRFToken": csrfToken
+      let nexturl = '/api/taxon/?limit=100&rank='+filterrank
+      while (nexturl != null) {
+        let response = await fetch(nexturl, {
+          headers: {
+            "X-CSRFToken": csrfToken
+          }
+        })
+        if (response.ok) {
+          let response_json = await response.json()
+          for (let child in response_json["results"]) {
+            let name = response_json["results"][child].taxonrank.latin_name
+            let taxid = response_json["results"][child].taxid
+            let rank = response_json["results"][child].taxonrank.rank
+            let pk = response_json["results"][child].taxonrank.pk
+            vm.inputs.opts.autoinput.taxid.options.push({
+                "taxid": taxid,
+                "label": "[" + rank[0].toUpperCase() + rank.slice(1) + "] " + name + " (taxid: " + taxid + ")",
+                "pk": pk
+              }
+            )
+          }
+          nexturl = response_json["next"]
+        } else {
+          this.$root.$data.modaltitle = 'Error'
+          this.$root.$data.modalmsg = await response.text()
+          this.$root.$data.modalvariant = 'danger'
+          this.$root.$emit('show-msg');
         }
-      })
-      if (response.ok) {
-        let response_json = await response.json()
-        for (let child in response_json) {
-          let name = response_json[child].taxonrank.latin_name
-          let taxid = response_json[child].taxid
-          let rank = response_json[child].taxonrank.rank
-          let pk = response_json[child].taxonrank.pk
-          vm.inputs.opts.autoinput.taxid.options.push({
-              "taxid": taxid,
-              "label": "[" + rank[0].toUpperCase() + rank.slice(1) + "] " + name + " (taxid: " + taxid + ")",
-              "pk": pk
-            }
-          )
-        }
-      } else {
-        this.$root.$data.modaltitle = 'Error'
-        this.$root.$data.modalmsg = await response.text()
-        this.$root.$data.modalvariant = 'danger'
-        this.$root.$emit('show-msg');
+        vm.inputs.sp.specificity_taxa.sp_taxid.options = vm.inputs.opts.autoinput.taxid.options
       }
-      vm.inputs.sp.specificity_taxa.sp_taxid.options = vm.inputs.opts.autoinput.taxid.options
     }
     vm.loading_taxa = false
   },
